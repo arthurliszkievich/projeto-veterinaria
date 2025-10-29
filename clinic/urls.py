257c3294870449d53e05148f7a2310329e96364e
Arthur Liszkievich <arthur.liszkievich@gmail.com>
2025-10-29 19:12:59 -0300

    feat(urls): adicionar rotas de autenticação
    - Adicionar path auth/register/ para registro de usuários
    - Adicionar path auth/user/ para obter dados do usuário autenticado

--- clinic/urls.py
+++ clinic/urls.py
@@ -1,12 +1,15 @@
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+
 from .views import (
-    TutorViewSet,
-    PacienteViewSet,
-    VeterinarioViewSet,
     ConsultaViewSet,
-    SintomaViewSet,
     DoencaViewSet,
+    PacienteViewSet,
+    SintomaViewSet,
+    TutorViewSet,
+    VeterinarioViewSet,
+    get_user_info,
+    register_user,
 )
 
 # Cria um router e registra viewsets com ele
@@ -16,8 +19,10 @@ router.register(r"pacientes", PacienteViewSet, basename="paciente")
 router.register(r"veterinarios", VeterinarioViewSet, basename="veterinario")
 router.register(r"consultas", ConsultaViewSet, basename="consulta")
 router.register(r"sintomas", SintomaViewSet, basename="sintoma")
-router.register(r'doencas', DoencaViewSet, basename='doenca')
+router.register(r"doencas", DoencaViewSet, basename="doenca")
 
 urlpatterns = [
+    path("auth/register/", register_user, name="register"),
+    path("auth/user/", get_user_info, name="user-info"),
     path("", include(router.urls)),
 ]
